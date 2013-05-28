1162be9b4c5487849696f36336ded515bceff20a
wanderanimrod <wanderanimrod@gmail.com>
2013-05-28 13:49:26 +0300

    [jmdb, nim] Tidying up interface to load_map

--- mapping/mapping.js
+++ mapping/mapping.js
@@ -9,6 +9,30 @@ function on_map_zoom(map) {
     console.log("[map-zoom] Current level: [" + map.zoom + "]");
 }
 
+function lonLatPoint(lon, lat) {
+    return {lon: lon, lat: lat};
+}
+
+/* 
+ left and right are longitute, top and bottom is lattitude
+ left, bottom, right, top --> bottomLeftPoint(Lon, Lat), topRightPoint(Lon, Lat)
+*/
+var ESPG_4326_PROJECTION = new OpenLayers.Projection("EPSG:4326"); // This is the standard lon lat projection
+var ESPG_900913_PROJECTION = new OpenLayers.Projection("EPSG:900913"); // This is google v3
+
+function lonLatBounds(bottomLeft, topRight) {
+   
+   return {bottomLeft: bottomLeft, 
+           topRight: topRight, 
+           espg900913:  new OpenLayers.Bounds(bottomLeft.lon, bottomLeft.lat, topRight.lon, topRight.lat)
+                               .transform(ESPG_4326_PROJECTION, ESPG_900913_PROJECTION),
+
+           lonLatString: "bottomLeft: (" + bottomLeft.lon + ", " + bottomLeft.lat + ") " 
+                       + "topRight: (" + topRight.lon + ", " +  topRight.lat + ")"
+                       + " [" + ESPG_4326_PROJECTION + "]"
+          };
+}
+
 function createMapContainer(divId) {
     var map = new OpenLayers.Map(divId, {controls: []});
   
@@ -23,47 +47,34 @@ function createMapContainer(divId) {
 }
 
 function createGoogleBaseLayer() {
-   var google_road_map_options = {
+    var google_road_map_options = {
         type: google.maps.MapTypeId.ROADMAP,
         numZoomLevels: 10          
     }
     return new OpenLayers.Layer.Google("Google road map", google_road_map_options);
 }
 
-function lonLatPoint(lon, lat) {
-    return {lon: lon, lat: lat};
-}
-
-function lonLatBounds(bottomLeft, topRight) {
-   return {bottomLeft: bottomLeft, 
-           topRight: topRight, 
-           espg900913:  new OpenLayers.Bounds(bottomLeft.lon, bottomLeft.lat, topRight.lon, topRight.lat)
-                               .transform(ESPG_4326_PROJECTION, ESPG_900913_PROJECTION),
-
-           lonLatString: "bottomLeft: (" + bottomLeft.lon + ", " + bottomLeft.lat + ") " 
-                       + "topRight: (" + topRight.lon + ", " +  topRight.lat + ")"
-                       + " [" + ESPG_4326_PROJECTION + "]"
-          };
-}
-
-/* 
- left and right are longitute, top and bottom is lattitude
- left, bottom, right, top --> bottomLeftPoint(Lon, Lat), topRightPoint(Lon, Lat)
-*/
-var ESPG_4326_PROJECTION = new OpenLayers.Projection("EPSG:4326"); // This is the standard lon lat projection
-var ESPG_900913_PROJECTION = new OpenLayers.Projection("EPSG:900913"); // This is google v3
-
+// function createPollLayer(options) {
+    
+//     poll_data_layer = new OpenLayers.Layer.WMS(layerName, wmsServerUrl,
+//         {
+//             VIEWPARAMS: 'poll:{{poll.pk}};app:{{deployment_id}}',
+//             LAYERS: layerName,
+//         },
+//         {
+//             isBaseLayer: false,
+//         }
+//     );
+// }
 
 // Note that the zoom level is automatically calculated by the size of the div and that of the bounding box
 // So if you need it zoomed in more, you may need to make the div bigger
-function load_map(divId, layerName, wmsServerUrl, initialBounds) {
-    console.log("Loading a map into div [" + divId + "] with layer [" + layerName + "] from [" + wmsServerUrl + "] ...");
+function load_map(divId, initialBounds, dataLayer) {
+    console.log("Loading a map into div [" + divId + "]");
   
     var map = createMapContainer(divId);
 
     map.addLayers([createGoogleBaseLayer()]);
-
-    
     
     map.zoomToExtent(initialBounds.espg900913);
 
