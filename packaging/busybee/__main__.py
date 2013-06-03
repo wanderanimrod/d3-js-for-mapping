2b6140bf9ab47638adc3dc85c7eae46b23f24127
Jim Barritt <jim@planet-ix.com>
2013-06-03 16:05:31 +0100

    [jmdb] Adding packaging spike

---
+++ packaging/busybee/__main__.py
@@ -0,0 +1,7 @@
+# http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
+
+def main():
+    print("Hello, welcome to my command line!!")
+
+if __name__ == '__main__':
+    main()
---
+++ packaging/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BusyBee',
+      version='1.0',
+      description='A simple test application for demonstrating debian packaging',
+      author='Jim Barritt',
+      author_email='jim@foobar.com',
+      url='http://foobar.com',
+      packages=['busybee'],
+     )
+
