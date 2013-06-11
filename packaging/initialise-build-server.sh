cfc05084e9b90ef907f8eeef7ae03233cfe2b33a
Jim Barritt <jim@planet-ix.com>
2013-06-11 12:18:38 +0100

    [jmdb] Adding a build tools script

--- packaging/initialise-build-server.sh
+++ packaging/initialise-build-server.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # To run this do
-# wget -O https://raw.github.com/ureport/spikes/master/packaging/initialise-build-server.sh 
-# ./initialise-build-server.sh
+# wget -O initialise-build-server.sh https://raw.github.com/ureport/spikes/master/packaging/initialise-build-server.sh; sh ./initialise-build-server.sh
+# 
 
 echo "Going to set up this server with everything you need to build debian packages..."
 
-sudo apt-get install git
+sudo apt-get install -y git
+
+mkdir ~/Code
+
+cd Code
+
+git clone git://github.com/ureport/spikes.git
+
+cd spikes/packaging
+
+./install-debian-build-tools.sh
 
 echo "Setup complete."
---
+++ packaging/install-debian-build-tools.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Going to install all the build tools we need..."
