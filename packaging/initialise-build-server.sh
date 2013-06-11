32e1299bc7278b48e72d12accb9033fe94d17ddf
Jim Barritt <jim@planet-ix.com>
2013-06-11 12:10:04 +0100

    [jmdb] Adding a setup script for the server

---
+++ packaging/initialise-build-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# To run this do
+# wget -O https://raw.github.com/ureport/spikes/master/packaging/initialise-build-server.sh 
+# ./initialise-build-server.sh
+
+echo "Going to set up this server with everything you need to build debian packages..."
+
+sudo apt-get install git
+
+echo "Setup complete."
