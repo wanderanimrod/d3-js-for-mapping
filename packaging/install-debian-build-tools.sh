7d25acf499b094cfd92d418529cba693b55be914
Jim Barritt <jim@planet-ix.com>
2013-06-11 12:24:19 +0100

    [jmdb] Adding various packages to the install

--- packaging/install-debian-build-tools.sh
+++ packaging/install-debian-build-tools.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# See http://www.debian.org/doc/manuals/maint-guide/start.en.html
+
 echo "Going to install all the build tools we need..."
+
+sudo apt-get install -y debhelper deb-make devscripts gnupg lintian patch patchutils pbuilder
