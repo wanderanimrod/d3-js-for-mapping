5c96940871087cc57d1048d623759520c0ac6003
Jim Barritt <jim@planet-ix.com>
2013-06-11 12:42:32 +0100

    [jmdb] Adding virtualenv to the script

--- packaging/install-debian-build-tools.sh
+++ packaging/install-debian-build-tools.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
 # See http://www.debian.org/doc/manuals/maint-guide/start.en.html
+# See http://www.cyberciti.biz/tips/linux-debian-package-management-cheat-sheet.html
+
+# To see what it installed try dpkg -l
 
 echo "Going to install all the build tools we need..."
 
 sudo apt-get update
 
 sudo apt-get install -y debhelper dh-make devscripts gnupg lintian patch patchutils pbuilder fakeroot file
+
+sudo apt-get install -y python-pip
+
+wget https://bitbucket.org/pypa/setuptools/raw/0.7.2/ez_setup.py -O - | sudo python
+
+sudo easy_install virtualenv
+
+mkdir ~/.virtualenvs
+
+cd ~/.virtualenvs
+
+virtualenv --no-site-packages packaging
+
+cd ~/Code/spikes/packaging
---
+++ packaging/pyenv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+source ~/.virtualenvs/packaging/bin/activate
