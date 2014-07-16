67d8482a8e4a8ccca0caa4c082e93adc8f6cb816
wanderanimrod <wanderanimrod@gmail.com>
2014-07-16 23:50:36 +0300

    Wonderful refactor

--- d3_maps/map.js
+++ d3_maps/map.js
@@ -1,31 +1,16 @@
-function DataPoint(district, score) {
-	this.district = district;
-	this.score = score;
-}
-
-function fetchData(districts) {
-	var dataPoints = [];
-	districts.forEach(function(district) {
-        var districtName = district.properties.name;
-        var randomScore = Math.random();
-        var dataPoint = new DataPoint(districtName, randomScore);
-		dataPoints.push(dataPoint); 
-	});
-	return dataPoints;
-}
-
 var width = 1500,
     height = 800;
+
+svg = d3.select("#map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 projection = d3.geo.mercator()
     .center([32.0, 1.35])
     .scale(7000)
     .translate([width/2, height/2]);
 
-svg = d3.select("#map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
+// Explain what the quantize scale does.
 color = d3.scale.quantize().range(["rgb(237,248,233)", "rgb(186,228,179)",
             "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"
             ]);
@@ -34,20 +19,7 @@ path = d3.geo.path().projection(projection);
 
 d3.json("districts.json", function(json) {
 
-    var data = fetchData(json.features);
-
-	data.forEach(function(dataPoint) {
-		var district = dataPoint.district;
-        var score = parseFloat(dataPoint.score);
-
-        for (var j = 0; j < json.features.length; j++) {
-	        var jsonDistrict = json.features[j].properties.name;
-	        if (district == jsonDistrict) {
-	            json.features[j].properties.value = score;
-	            break;
-	        }
-        }
-	});
+    addRandomDataToDistricts(json.features);
 
 	svg.selectAll("path")
 		.data(json.features)
@@ -59,3 +31,10 @@ d3.json("districts.json", function(json) {
            	return color(value);
     	});
 });
+
+function addRandomDataToDistricts(districts) {
+    return districts.map(function(district) {
+        district.properties.value = Math.random();
+        return district
+    })
+}
