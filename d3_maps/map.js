df844d0079a8059feaf4245327223e5e9acf5804
wanderanimrod <wanderanimrod@gmail.com>
2014-07-16 23:32:43 +0300

    Blip and map. Blip not correctly positioned

--- d3_maps/map.js
+++ d3_maps/map.js
@@ -1,25 +1,29 @@
 var width = 1500,
     height = 800;
 
-projection = d3.geo.mercator()
-    .center([32.0, 1.35])
-    .scale(7000)
-    .translate([width/2, height/2]);
-
 svg = d3.select("#map").append("svg")
     .attr("width", width)
     .attr("height", height);
+
+projection = d3.geo.mercator()
+    .center([32.0, 1.35])
+    .scale(7000)
+    .translate([width / 2, height / 2]);
 
 path = d3.geo.path().projection(projection);
 
 d3.json("districts.json", function(json) {
 
-	svg.selectAll("path")
-		.data(json.features)
+    svg.selectAll("path")
+        .data(json.features)
+        // When all path elements have been selected and there is left over elements in features, create a path dom element for each feature
         .enter()
-		.append("path")
-		.attr("d", path)
-        .attr("fill", function() {
-            return "rgb(0,0,0)";
-        });
+        .append("path")
+        .attr("d", path);
 });
+
+svg.append("circle")
+    .attr("r", 10)
+    .attr("cx", 100)
+    .attr("cy", 100)
+    .attr("fill", "red");
